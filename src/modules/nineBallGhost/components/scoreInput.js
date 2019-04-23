2552955d3e712e9fbaf8cb61e8babb834dabe93c
sretundijr <sretundijr@gmail.com>
2019-04-23 00:04:34 -0600

    bug fix checkbox button changing size when both are rendered

--- src/modules/nineBallGhost/components/scoreInput.js
+++ src/modules/nineBallGhost/components/scoreInput.js
@@ -10,18 +10,20 @@ const ScoreInput = (props) => {
     <div className='scoreInput'>
       <div className='scoreInput-ballCountLabel'>Number of Balls Pocketed</div>
       <BallCounter {...props} />
-      <CheckBoxButton
-        label='Was the Nine Ball pocketed'
-        onClick={props.actions.nineBallClick}
-        checked={props.nineBallChecked}
-      />
-      {props.nineBallChecked &&
+      <div className='scoreInput-checkboxContainer'>
         <CheckBoxButton
-          label='Was the nine ball pocketed on the break?'
-          onClick={props.actions.nineBallBreakClick}
-          checked={props.nineBallBreak}
+          label='Was the Nine Ball pocketed'
+          onClick={props.actions.nineBallClick}
+          checked={props.nineBallChecked}
         />
-      }
+        {props.nineBallChecked &&
+          <CheckBoxButton
+            label='Was the nine ball pocketed on the break?'
+            onClick={props.actions.nineBallBreakClick}
+            checked={props.nineBallBreak}
+          />
+        }
+      </div>
     </div>
   )
 }
