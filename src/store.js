cbe318eb7bb13cbe26b477b6fa4544fa4a5f5dc4
sretundijr <sretundijr@gmail.com>
2019-04-28 16:40:49 -0600

    redux boilerplate

--- src/store.js
+++ src/store.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import NineBallGhostReducer from './modules/nineBallGhost/redux/reducers';
+import ModalReducer from './uiComponents/modals/redux/reducers';
 
 const reducers = combineReducers({
-  NineBallGhostState: NineBallGhostReducer
+  NineBallGhostState: NineBallGhostReducer,
+  ModalState: ModalReducer
 });
 
 const store = createStore(reducers, applyMiddleware(thunk));
